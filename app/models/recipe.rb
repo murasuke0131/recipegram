998be93c5ed37c668d71f9cf8f3ecabe24dbaa34
村原亮介 <murasukemac@gmail.com>
2021-07-04 14:51:37 +0900

    first commit

---
+++ app/models/recipe.rb
@@ -0,0 +1,10 @@
+class Recipe < ApplicationRecord
+    belongs_to :user
+    has_many :favorites,dependent: :destroy
+
+
+    with_options presence: true do
+        validates :title
+        validates :body
+    end
+end
